9fb26e9e458a51a41b8a8277bd9b1f047e7ca618
Kirill Lossenko <kiloss@ttu.ee>
2020-09-13 16:29:50 +0300

    init project

---
+++ MenuSystem/Menu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenuSystem
+{
+    public class Menu
+    {
+        public List<MenuItem> MenuItems { get; set; }
+
+        public Menu()
+        {
+            MenuItems = new List<MenuItem>();
+            MenuItems.Add(new MenuItem("new game player vs player", "1"));
+            MenuItems.Add(new MenuItem("New game person vs AI", "2"));
+            MenuItems.Add(new MenuItem("new game AI vs AI", "3"));
+        }
+
+        public void RunMenu()
+        {
+            var userChoice = "";
+            do
+            {
+                Console.Write("");
+                foreach (var menuItem in MenuItems)
+                {
+                    Console.WriteLine(menuItem);
+                }
+                
+                Console.WriteLine("L) Load game");
+                Console.WriteLine("X) exit");
+                Console.Write(">");
+                
+                userChoice = Console.ReadLine()?.ToLower().Trim() ?? "";
+
+                Console.WriteLine("Your Choice was:" + userChoice);
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.WriteLine("not implemented yet");
+                        break;
+                
+                    case "2":
+                        Console.WriteLine("not implemented yet");
+                        break;
+                
+                    case "3":
+                        Console.WriteLine("not implemented yet");
+                        break;
+                
+                    case "l":
+                        Console.WriteLine("not implemented yet");
+                        break;
+                    case "x":
+                        Console.WriteLine("closing down");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, no option matches input");
+                        break;
+                }    
+            } while (userChoice != "x");
+        
+        }
+    }
+}
---
+++ ics0016-2020f/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MenuSystem;
+
+namespace ica0016_2020f
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============> TIC-TAC-TOE KILOSS <=================");
+
+            var menu = new Menu();
+            menu.RunMenu();
+            
+        }
+    }
+}
