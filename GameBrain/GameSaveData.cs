3c7603f5bcc0a897dde6dd2762c9a05f64335ed5
cybercubes <cyberandcubes@gmail.com>
2020-11-05 22:20:10 +0200

    added missing class

---
+++ GameBrain/GameSaveData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GameBrain
+{
+    public class GameSaveData
+    {
+        public int GameSaveDataId { get; set; }
+        
+        public string TimeStamp { get; set; } = DateTime.Now.ToLongDateString();
+        
+        public string SerializedGameData { get; set; }
+        
+        public int GameOptionsId { get; set; }
+        public GameOption GameOption { get; set; }
+        
+        
+    }
+}
